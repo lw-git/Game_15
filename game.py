c77595f3b9c1ef4b1b0f9dd5a66ccadc73c155d5
lw <lw_git@mail.ru>
2020-07-25 10:27:10 +0500

    Added cells

--- game.py
+++ game.py
@@ -1,4 +1,26 @@
 import tkinter as tk
+import random
+
+
+class Cell():
+    def __init__(self, canvas, size, color, number, x, y):
+        self.cell_id = None
+        self.text_id = None
+        self.canvas = canvas
+        self.size = size
+        self.color = color
+        self.number = number
+        self.x = x * self.size
+        self.y = y * self.size
+        self.create()
+
+    def create(self):
+        self.cell_id = self.canvas.create_rectangle(
+            self.x, self.y, self.x + self.size, self.y + self.size,
+            fill=self.color)
+        self.text_id = self.canvas.create_text(
+            self.x + self.size // 2, self.y + self.size // 2,
+            text=self.number, justify=tk.CENTER, font="Consolas 25")
 
 
 class Application(tk.Frame):
@@ -6,13 +28,32 @@ class Application(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs)
 
-        # -----------------Variables-----------------------------
+        # ---------------------Variables-------------------------
         self.cell_size = 150
+        self.cells = []
+        self.colors = ["green", "lightgreen", "gray", "silver",
+                       "spring green", "rosybrown", "lawngreen", "deep pink",
+                       "cyan", "lightblue", "lime", "violet",
+                       "gold", "orange", "firebrick1", "deepskyblue"]
+        self.numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9',
+                        '10', '11', '12', '13', '14', '15']
+        self.freecell = None
 
-        # -----------------Widgets-------------------------------
+        # -----------------------Widgets-------------------------
         self.canvas = tk.Canvas(root, width=4 * self.cell_size,
                                 height=4 * self.cell_size, bg='white')
         self.canvas.pack()
+
+        # ------------------------Start--------------------------
+        self.create_cells()
+
+    def create_cells(self):
+        numbers = self.numbers[:]
+        for i in range(0, 15):
+            number = numbers.pop(random.randint(0, len(numbers) - 1))
+            self.cells.append(Cell(self.canvas, self.cell_size,
+                                   random.choice(self.colors),
+                                   number, i % 4, i // 4))
 
 
 if __name__ == '__main__':
